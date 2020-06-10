dfeaa4a215f3d8e569bf91b3b57fa3862f650c3c
Quentin <quentin.mendel1@gmail.com>
2020-06-10 09:29:23 +0200

    First Commit, creation of project

---
+++ Devoir_Final/main.py
@@ -0,0 +1,5 @@
+def main ():
+    print("Bienvenu, dans ce script qui récupère une image précise et qui va insérer dans celle-ci, une message codé en binaire, choisis au préalable")
+
+if __name__ == '__main__':
+    main()
